369c93a72d4ce45ab5405e6fef2ba609b62313b0
Manohisoa <manohisoarajaonarivony@gmail.com>
2022-12-15 16:30:17 +0300

    added home page components and styling

--- src/sections/HomeSection/HomeSection.jsx
+++ src/sections/HomeSection/HomeSection.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
+import profilePicture from "../../assets/profilePicture.jpeg"
 
 import "./HomeSection.css"
 
 function HomeSection() {
   return (
     <section id="home-section">
-      <h2>HOME</h2>
+      <div id="home-container">
+        <div id="image-container">
+          <img src={profilePicture} alt='profile' />
+        </div>
+        <div id="intro-container">
+          <h1>Hi, I'm Manohisoa Rajaonarivony</h1>
+          <h3>Junior Full Stack Software Developer</h3>
+        </div>
+      </div>
     </section>
   )
 }
