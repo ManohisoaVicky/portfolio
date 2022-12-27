9c279c7d4053ff7ded91c8ccd30293e2c6b97589
Manohisoa <manohisoarajaonarivony@gmail.com>
2022-12-27 17:15:41 +0300

    fixed experience card going over navbar

---
+++ src/data/projects.js
@@ -0,0 +1,56 @@
+export const projects = [
+  {
+    name: "Eventech",
+    link: "no link yet",
+    readme_link: "no link yet",
+    tech_stack: [
+      {
+        icon: "devicon-javascript-plain",
+        name: "JavaScript",
+      },
+      {
+        icon: "devicon-react-original",
+        name: "ReactJS",
+      },
+      {
+        icon: "devicon-html5-plain",
+        name: "HTML5",
+      },
+      {
+        icon: "devicon-css3-plain",
+        name: "CSS",
+      },
+      {
+        icon: "devicon-python-plain",
+        name: "Python",
+      },
+      {
+        icon: "devicon-django-plain",
+        name: "Django",
+      },
+      {
+        icon: "devicon-postgresql-plain",
+        name: "PostgreSQL",
+      },
+      {
+        icon: "devicon-visualstudio-plain",
+        name: "VS Code",
+      },
+      {
+        icon: "devicon-git-plain",
+        name: "Git",
+      },
+      {
+        icon: "devicon-github-original",
+        name: "GitHub",
+      },
+      {
+        icon: "devicon-npm-original-wordmark",
+        name: "npm",
+      },
+    ],
+    timeframe: "3 weeks",
+    numOfPeople: "Individual",
+    description: "",
+  },
+];
