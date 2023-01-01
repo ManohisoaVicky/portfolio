8d41b0bf2eb215812f56747a0019c355362f35d5
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-01-01 21:53:01 +0300

    added contact info and disabled submit if form is empty

--- src/sections/ContactSection/ContactSection.jsx
+++ src/sections/ContactSection/ContactSection.jsx
@@ -1,11 +1,30 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import emailjs from "@emailjs/browser";
 
 import "./ContactSection.css";
 
 function ContactSection({ state }) {
+
+  const [inputState, setInputState] = useState({
+    user_name: "", 
+    user_email: "", 
+    message: ""
+  })
+
+  function isEmpty(input) {
+    return input.trim() !== "";
+  }
+
+  function minLength(word, char) {
+    return word.length > char;
+  }
+
+  function validateEmail(input) {
+    return /\S+@\S+\.\S+/.test(input);
+  }
+
     const form = useRef();
-    
+
     const sendEmail = (e) => {
       e.preventDefault();
 
@@ -17,32 +36,61 @@ function ContactSection({ state }) {
           process.env.REACT_APP_PUBLIC_KEY
         )
         .then(
-          (result) => {
-            console.log(result.text);
-          },
           (error) => {
             console.log(error.text);
           }
         );
+
+        setInputState({
+          user_name: "",
+          user_email: "",
+          message: "",
+        });
     };
 
+     const handleChange = (e) => {
+       setInputState((oldState) => ({
+         ...oldState,
+         [e.target.name]: e.target.value,
+       }));
+     };
+
+     let formIsInvalid = !(isEmpty(inputState.user_name) && validateEmail(inputState.user_email) && minLength(inputState.message, 15)
+)
   return (
     <section
       id="contact-section"
       className={!state.contact ? "hide" : undefined}
     >
-      <h2>Contacts</h2>
+      <h2>Contact Me</h2>
       <div id="contacts-container">
-        <div className="contact-form-container">
+        <div id="contact-form-container">
           <form ref={form} onSubmit={sendEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" />
-            <label>Email</label>
-            <input type="email" name="user_email" />
-            <label>Message</label>
-            <textarea name="message" />
-            <input type="submit" value="Send" />
+            <input type="text" name="user_name" placeholder="Name" value={inputState.user_name || ""} onChange={handleChange} />
+            <input type="email" name="user_email" placeholder="Email" value={inputState.user_email || ""} onChange={handleChange} />
+            <textarea name="message" placeholder="Message" value={inputState.message || ""} onChange={handleChange} />
+            <input type="submit" value="Send" disabled={formIsInvalid}/>
           </form>
+        </div>
+        <div id="contact-container">
+          <h3>Contact Info</h3>
+          <a href="mailto:manohisoarajaonarivony@gmail.com">
+            manohisoarajaonarivony@gmail.com
+          </a>
+          <a
+            href="https://www.linkedin.com/in/manohisoa-rajaonarivony/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            linkedin.com/in/manohisoa-rajaonarivony
+          </a>
+          <a
+            href="https://github.com/ManohisoaVicky"
+            target="_blank"
+            rel="noreferrer"
+          >
+            github.com/ManohisoaVicky
+          </a>
         </div>
       </div>
     </section>
