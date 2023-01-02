ba8a0a730e6f1c9368a44e1596cc8cd9b8fabc53
Manohisoa <manohisoarajaonarivony@gmail.com>
2023-01-02 19:33:01 +0300

    made navbar responsive

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { UilBars } from "@iconscout/react-unicons";
 
+import logo from "../../assets/logo.png"
 import "./NavBar.css"
 
 function NavBar({state, setState}) {
+
+  const [clicked, setClicked] = useState(false)
+
+  const handleClick = () => {
+    setClicked((prevState) => !prevState)
+  }
 
   const toAbout = () => {
     setState((state) => {
@@ -36,14 +44,45 @@ function NavBar({state, setState}) {
 
   return (
     <nav id="navbar-container">
-        <ul id="navbar-list-container">
-            <li><a href="#home-section" className='navbar-link'>HOME</a></li>
-            <li onClick={toAbout}><a href="#about-section" className='navbar-link'>ABOUT</a></li>
-            <li onClick={toProjects}><a href="#projects-section" className='navbar-link'>PROJECTS</a></li>
-            <li onClick={toContact}><a href="#contact-section" className='navbar-link'>CONTACT</a></li>
-        </ul>
+      <ul
+        className={
+          clicked ? "navbar-list-container extended" : "navbar-list-container"
+        }
+      >
+        <li>
+          <a href="#home-section" className="navbar-link">
+            HOME
+          </a>
+        </li>
+        <li onClick={toAbout}>
+          <a href="#about-section" className="navbar-link">
+            ABOUT
+          </a>
+        </li>
+        <li onClick={toProjects}>
+          <a href="#projects-section" className="navbar-link">
+            PROJECTS
+          </a>
+        </li>
+        <li onClick={toContact}>
+          <a href="#contact-section" className="navbar-link">
+            CONTACT
+          </a>
+        </li>
+      </ul>
+      <ul className="mobile-nav-list-container">
+        <a href="#home-section" className="navbar-link">
+          <img src={logo} alt="logo" id="logo-image" />
+        </a>
+        <UilBars
+          className={clicked ? "menu" : undefined}
+          size="50"
+          color="white"
+          onClick={handleClick}
+        />
+      </ul>
     </nav>
-  )
+  );
 }
 
 export default NavBar
